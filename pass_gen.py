e7de26794c3aeebab16874ebc811f45ab057da7d
dudushy <eduardotalarico1337@gmail.com>
2021-08-09 18:59:37 -0300

    a more elegant version

--- pass_gen.py
+++ pass_gen.py
@@ -10,7 +10,22 @@ password = ''
 
 # main
 os.system('cls') # 'cls' for windows, 'clear' for linux/android
+
+for i in range(18):
+    lowerChar = lowers[random.randrange(len(lowers))]
+    upperChar = uppers[random.randrange(len(uppers))]
+    numberChar = numbers[random.randrange(len(numbers))]
+    symbolChar = symbols[random.randrange(len(symbols))]
+
+    char = random.choices([lowerChar, upperChar, numberChar, symbolChar],[25, 25, 25, 25], k=1)
+
+    password += char[0]
+print(f'Strong password: {password}\n')
+
+os.system('pause')
+
+"""
 for char in range(18):
     password += random.choices([lowers[random.randrange(len(lowers))], uppers[random.randrange(len(uppers))], numbers[random.randrange(len(numbers))], symbols[random.randrange(len(symbols))]],[25, 25, 25, 25], k=1)[0]
-print(f'Strong password: {password}\n')
-os.system('pause')
+    print(f'Strong password: {password}\n')
+"""
